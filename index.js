41a1ac497d11231ae27f78cc61b269d2727c0a0d
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-04 03:55:13 +0600

    Using Mongo Find Multple Operation Add data from JSON to database and load on the client UI

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // midleware
 app.use(cors());
-app.use(express())
+app.use(express());
 
 // MongoDb Connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpr3cem.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productCollection = client.db('emajhon').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
 
 // Basic setup
 app.get('/', (req, res) => {
