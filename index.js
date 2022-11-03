7c724a26464806b097fea972dcad5bc133bafd69
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-04 02:02:57 +0600

    dynamically set MongoDB userName and Password

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 // midleware
 app.use(cors());
 app.use(express())
 
+// MongoDb Connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpr3cem.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// Basic setup
 app.get('/', (req, res) => {
     res.send('emazon simple server running');
 })
